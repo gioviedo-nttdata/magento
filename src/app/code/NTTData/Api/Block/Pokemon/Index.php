437abcec368f788817990481961edc4b1c095557
gioviedo-nttdata <gioviedo@emeal.nttdata.com>
2023-08-15 14:46:32 -0300

    Ejercicio VIII - Stores

--- src/app/code/NTTData/Api/Block/Pokemon/Index.php
+++ src/app/code/NTTData/Api/Block/Pokemon/Index.php
@@ -6,15 +6,18 @@ class Index extends \Magento\Framework\View\Element\Template
 {
 	protected $_pokeApi;
 	protected $_helper;
+	protected $_storeManager;
 
 	public function __construct(\Magento\Framework\View\Element\Template\Context $context, 
 		\NTTData\Api\Service\Api\PokeApi $pokeApi, 
-		\NTTData\Api\Helper\Data $helper)
+		\NTTData\Api\Helper\Data $helper,
+		\Magento\Store\Model\StoreManagerInterface $storeManager)
 	{
 		parent::__construct($context);
 
 		$this->_pokeApi = $pokeApi;
 		$this->_helper = $helper;
+		$this->_storeManager = $storeManager;
 	}
 
 	public function getDataPokemon(){
@@ -25,6 +28,10 @@ class Index extends \Magento\Framework\View\Element\Template
 
 	public function isEnabled(){
 		return $this->_helper->getEnabled();
+	}
+
+	public function showInStore(){
+		return  $this->_storeManager->getStore()->getCode() == 'pokemon';
 	}
 
 }
